d4fb24094f0098a7b3671135654205368ebf31af
Robin <robin@chilio.net>
2021-03-19 22:01:17 +0100

    app: Implement min endpoint

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, Response
 from flask.json import jsonify
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine, func
 from sqlalchemy.orm import sessionmaker
 import json
 import sqlite3
@@ -11,13 +11,13 @@ Base = declarative_base()
 
 class Reading(Base):
     __tablename__ = 'readings'
-
-    device_uuid = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    device_uuid = Column(String)
     type = Column(String)
     value = Column(Integer)
     date_created = Column(Integer)
 
-HTTP_UNOROCESSABLE_ENTITY = 422
+HTTP_UNPROCESSABLE_ENTITY = 422
 SENSOR_MIN = 0
 SENSOR_MAX = 100
 VALID_SENSOR_RANGE = range(SENSOR_MIN,SENSOR_MAX+1)
@@ -27,11 +27,14 @@ VALID_SENSOR_TYPE = ['temperature',
 app = Flask(__name__)
 
 # Setup the SQLite DB
-engine = create_engine("sqlite:///database.db")
-session = sessionmaker(bind=engine)
-conn = sqlite3.connect('database.db')
-conn.execute('CREATE TABLE IF NOT EXISTS readings (device_uuid TEXT, type TEXT, value INTEGER, date_created INTEGER)')
-conn.close()
+session = None
+
+def get_db_session():
+    if app.config['TESTING']:
+        engine = create_engine("sqlite:///test_database.db")
+    else:
+        engine = create_engine("sqlite:///database.db")
+    return sessionmaker(bind=engine)()
 
 @app.route('/devices/<string:device_uuid>/readings/', methods = ['POST', 'GET'])
 def request_device_readings(device_uuid):
@@ -51,25 +54,26 @@ def request_device_readings(device_uuid):
     """
 
     # Set the db that we want and open the connection
-    if app.config['TESTING']:
-        conn = sqlite3.connect('test_database.db')
-    else:
-        conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    cur = conn.cursor()
-    post_data = json.loads(request.data)
-    if request.method == 'POST':
-        # Grab the post parameters
-        sensor_type = post_data.get('type')
-        value = post_data.get('value')
-        date_created = post_data.get('date_created', int(time.time()))
+    with app.app_context():
+        session = get_db_session()
 
-        if sensor_type not in VALID_SENSOR_TYPE:
-            return HTTP_UNOROCESSABLE_ENTITY, ('Invalid sensor type. Needs to be one of: '
-                                              f'{VALID_SENSOR_TYPE}')
-        if value not in VALID_SENSOR_RANGE:
-            return HTTP_UNOROCESSABLE_ENTITY, ('Invalid sensor range. Needs to be in: '
-                                              f'{VALID_SENSOR_RANGE}')
+    data = {}
+    if len(request.data) > 0:
+        data = json.loads(request.data)
+    
+    if request.method == 'POST':
+        if len(data) == 0:
+            return 'Missing Payload', HTTP_UNPROCESSABLE_ENTITY
+        # Grab the post parameters
+        sensor_type = data.get('type')
+        value = data.get('value')
+        date_created = data.get('date_created', int(time.time()))
+        if sensor_type is None or sensor_type not in VALID_SENSOR_TYPE:
+            return ('Invalid sensor type. Needs to be one of: '
+                    f'{VALID_SENSOR_TYPE}'), HTTP_UNPROCESSABLE_ENTITY
+        if value is None or value not in VALID_SENSOR_RANGE:
+            return ('Invalid sensor range. Needs to be in: '
+                    f'{VALID_SENSOR_RANGE}'), HTTP_UNPROCESSABLE_ENTITY
         # Insert data into db
         reading = Reading(device_uuid=device_uuid,
                           type=sensor_type,
@@ -82,15 +86,57 @@ def request_device_readings(device_uuid):
         return 'success', 201
     else:
         # Execute the query
-        sensor_type = post_data.get('type')
-        start = post_data.get('start', 0)
-        end = post_data.get('end', int(time.time()))
-        session.query(Reading).filter_by(type=sensor_type, start=start, end=end).all()
-        cur.execute('select * from readings where device_uuid="{}"'.format(device_uuid))
-        rows = cur.fetchall()
+        sensor_type = data.get('type')
+        start = data.get('start')
+        end = data.get('end')
+        query = session.query(Reading).filter(Reading.device_uuid==device_uuid)
+        if sensor_type is not None:
+            query = query.filter(Reading.type==sensor_type)
+        if start is not None:
+            query = query.filter(Reading.date_created >= start)
+        if end is not None:
+            query = query.filter(Reading.date_created <= end)
 
         # Return the JSON
-        return jsonify([dict(zip(['device_uuid', 'type', 'value', 'date_created'], row)) for row in rows]), 200
+        return jsonify([{'device_uuid': row.device_uuid,
+                         'type': row.type,
+                         'value': row.value,
+                         'date_created': row.date_created} for row in query.all()]), 200
+
+@app.route('/devices/<string:device_uuid>/readings/min/', methods = ['GET'])
+def request_device_readings_min(device_uuid):
+    """
+    This endpoint allows clients to GET the min sensor reading for a device.
+
+    Mandatory Query Parameters:
+    * type -> The type of sensor value a client is looking for
+
+    Optional Query Parameters
+    * start -> The epoch start time for a sensor being created
+    * end -> The epoch end time for a sensor being created
+    """
+    data = {}
+    if len(request.data) > 0:
+        data = json.loads(request.data)
+    sensor_type = data.get('type')
+    if sensor_type is None:
+        return 'Missing Payload', HTTP_UNPROCESSABLE_ENTITY
+
+    with app.app_context():
+        session = get_db_session()
+
+    query = session.query(func.min(Reading.value), Reading.date_created) \
+                   .filter(Reading.device_uuid==device_uuid) \
+                   .filter(Reading.type==sensor_type)
+    if data.get('start') is not None:
+        query = query.filter(Reading.date_created >= data.get('start'))
+    if data.get('end') is not None:
+        query = query.filter(Reading.date_created <= data.get('end'))
+    
+    return jsonify([{'device_uuid': device_uuid,
+                     'type': sensor_type,
+                     'value': query.one()[0],
+                     'date_created': query.one()[1]} for row in query.all()]), 200
 
 @app.route('/devices/<string:device_uuid>/readings/max/', methods = ['GET'])
 def request_device_readings_max(device_uuid):
@@ -104,8 +150,14 @@ def request_device_readings_max(device_uuid):
     * start -> The epoch start time for a sensor being created
     * end -> The epoch end time for a sensor being created
     """
-
-    return 'Endpoint is not implemented', 501
+    with app.app_context():
+        session = get_db_session()
+    type = 'temperature'
+    query = session.query(func.min(Reading.value), Reading.date_created).filter(Reading.device_uuid==device_uuid)
+    return jsonify([{'device_uuid': device_uuid,
+                     'type': type,
+                     'value': query.one()[0],
+                     'date_created': query.one()[1]} for row in query.all()]), 200
 
 @app.route('/devices/<string:device_uuid>/readings/median/', methods = ['GET'])
 def request_device_readings_median(device_uuid):
