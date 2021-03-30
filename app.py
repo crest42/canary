9ffef697086e8ff129842e41dd15d0d94a41089f
Robin <robin@chilio.net>
2021-03-31 00:44:27 +0200

    app.py: Implement summary endpoint and fix return types of multiple endpoints

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from jsonschema import validate, ValidationError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, create_engine, func, distinct, text
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import MultipleResultsFound
 
 Base = declarative_base()
 
@@ -64,14 +65,14 @@ app = Flask(__name__)
 app.config['TESTING'] = True
 
 def normalize_quartiles(q):
-    print(q)
     assert(len(q) <= 4)
+    assert(len(q) > 0)
     if len(q) == 1:
-        return [(1,q[0][1]), (2,q[0][1]), (3,q[0][1]), (4,q[0][1])]
+        return [(1,) + q[0][1:], (2,) + q[0][1:], (3,) + q[0][1:], (4,) + q[0][1:]]
     if len(q) == 2:
-        return [(1,q[0][1]), (2,q[0][1]), (3,q[1][1]), (4,q[1][1])]
+        return [(1,) + q[0][1:], (2,) + q[0][1:], (3,) + q[1][1:], (4,) + q[1][1:]]
     if len(q) == 3:
-        return [(1,q[0][1]), (2,q[1][1]), (3,q[2][1]), (4,q[2][1])]
+        return [(1,) + q[0][1:], (2,) + q[1][1:], (3,) + q[2][1:], (4,) + q[2][1:]]
     return [q[0], q[1], q[2], q[3]]
 
 def get_db_session():
@@ -144,11 +145,16 @@ def request_device_readings(device_uuid):
         if end is not None:
             query = query.filter(Reading.date_created <= end)
 
+
+        result = query.all()
+        if len(result) == 0:
+            return jsonify([]), 200
+
         # Return the JSON
         return jsonify([{'device_uuid': row.device_uuid,
                          'type': row.type,
                          'value': row.value,
-                         'date_created': row.date_created} for row in query.all()]), 200
+                         'date_created': row.date_created} for row in result]), 200
 
     return (f'Invalid request method {request.method}'), HTTP_UNPROCESSABLE_ENTITY
 
@@ -208,10 +214,19 @@ def request_device_readings_min(device_uuid):
     if end_date is not None:
         query = query.filter(Reading.date_created <= end_date)
 
-    return jsonify([{'device_uuid': device_uuid,
+    result = None
+    try:
+        result = query.one_or_none()
+    except MultipleResultsFound as exception:
+        return (f'Internal Server Error: {exception}'), 500
+
+    if result[0] is None:
+        return jsonify({}), 200
+
+    return jsonify({'device_uuid': device_uuid,
                      'type': sensor_type,
-                     'value': query.one()[0],
-                     'date_created': query.one()[1]} for row in query.all()]), 200
+                     'value': result[0],
+                     'date_created': result[1]}), 200
 
 @app.route('/devices/<string:device_uuid>/readings/max/', methods = ['GET'])
 def request_device_readings_max(device_uuid):
@@ -251,10 +266,20 @@ def request_device_readings_max(device_uuid):
         query = query.filter(Reading.date_created >= start_date)
     if end_date is not None:
         query = query.filter(Reading.date_created <= end_date)
-    return jsonify([{'device_uuid': device_uuid,
+
+    result = None
+    try:
+        result = query.one_or_none()
+    except MultipleResultsFound as exception:
+        return (f'Internal Server Error: {exception}'), 500
+
+    if result[0] is None:
+        return jsonify({}), 200
+
+    return jsonify({'device_uuid': device_uuid,
                      'type': sensor_type,
-                     'value': query.one()[0],
-                     'date_created': query.one()[1]} for row in query.all()]), 200
+                     'value': result[0],
+                     'date_created': result[1]}), 200
 
 @app.route('/devices/<string:device_uuid>/readings/median/', methods = ['GET'])
 def request_device_readings_median(device_uuid):
@@ -287,19 +312,25 @@ def request_device_readings_median(device_uuid):
     with app.app_context():
         session = get_db_session()
 
-    query = session.query(Reading.value, Reading.date_created) \
-                   .filter(Reading.device_uuid==device_uuid) \
-                   .filter(Reading.type==sensor_type)
+    quartile_cte = session.query(Reading.date_created, Reading.value, func.ntile(4).over(order_by=Reading.value).label('quartiles'))\
+                          .filter(Reading.device_uuid==device_uuid)\
+                          .filter(Reading.type==sensor_type)
     if start_date is not None:
-        query = query.filter(Reading.date_created >= start_date)
+        quartile_cte = quartile_cte.filter(Reading.date_created >= start_date)
     if end_date is not None:
-        query = query.filter(Reading.date_created <= end_date)
-    a = query.all()
-    i = list(zip(*a))[0].index(numpy.quantile(list(zip(*a))[0], q=0.5, interpolation='nearest'))
-    return jsonify([{'device_uuid': device_uuid,
+        quartile_cte = quartile_cte.filter(Reading.date_created <= end_date)
+    quartile_cte = quartile_cte.cte('p')
+    query = session.query(quartile_cte.c.quartiles, func.max(quartile_cte.c.value), quartile_cte.c.date_created,).group_by(quartile_cte.c.quartiles)
+    result = query.all()
+    if len(result) == 0:
+        return jsonify({}), 200
+
+    quartiles = normalize_quartiles(result)
+
+    return jsonify({'device_uuid': device_uuid,
                      'type': sensor_type,
-                     'value': a[i][0],
-                     'date_created': a[i][1]}]), 200
+                     'value': quartiles[1][1],
+                     'date_created': quartiles[1][2]}), 200
 
 @app.route('/devices/<string:device_uuid>/readings/mean/', methods = ['GET'])
 def request_device_readings_mean(device_uuid):
@@ -333,14 +364,24 @@ def request_device_readings_mean(device_uuid):
     with app.app_context():
         session = get_db_session()
 
-    query = session.query(func.avg(Reading.value)) \
+    query = session.query(func.round(func.avg(Reading.value),2)) \
                    .filter(Reading.device_uuid==device_uuid) \
                    .filter(Reading.type==sensor_type)
     if start_date is not None:
         query = query.filter(Reading.date_created >= start_date)
     if end_date is not None:
         query = query.filter(Reading.date_created <= end_date)
-    return jsonify([{'value': query.one()[0]} for row in query.all()]), 200
+
+    result = None
+    try:
+        result = query.one_or_none()
+    except MultipleResultsFound as exception:
+        return (f'Internal Server Error: {exception}'), 500
+
+    if result[0] is None:
+        return jsonify({}), 200
+
+    return jsonify({'value': result[0]}), 200
 
 request_device_readings_quartiles_schema = {
    'type': 'object',
@@ -399,8 +440,8 @@ def request_device_readings_quartiles(device_uuid):
     query = session.query(quartile_cte.c.quartiles, func.max(quartile_cte.c.value), ).group_by(quartile_cte.c.quartiles)
     quartiles = normalize_quartiles(query.all())
 
-    return jsonify([{'quartile_1': quartiles[0][1],
-                     'quartile_3': quartiles[2][1]}]), 200
+    return jsonify({'quartile_1': quartiles[0][1],
+                     'quartile_3': quartiles[2][1]}), 200
 
 request_summary_schema = {
    'type': 'object',
@@ -453,7 +494,7 @@ def request_readings_summary():
     query = session.query(Reading.device_uuid,
                           func.max(Reading.value),
                           func.min(Reading.value),
-                          func.avg(Reading.value),
+                          func.round(func.avg(Reading.value),2),
                           func.count(),).\
                     group_by(Reading.device_uuid)
     if sensor_type is not None:
@@ -471,7 +512,6 @@ def request_readings_summary():
         quartile_dict.setdefault(device_uuid, []).append((quartile,value))
     quartiles = dict(map(lambda x: (x[0], normalize_quartiles(x[1])), quartile_dict.items()))
 
-
     return jsonify([{'device_uuid': value,
                          'max_reading_value': aggregates[value][0],
                          'min_reading_value': aggregates[value][1],
@@ -480,7 +520,7 @@ def request_readings_summary():
                          'quartile_1_value': quartiles[value][0][1],
                          'median_reading_value': quartiles[value][1][1],
                          'quartile_3_value': quartiles[value][1][1],
-                         } for value in aggregates]), 200
+                         } for value in aggregates.keys()]), 200
 
 if __name__ == '__main__':
     app.run()
